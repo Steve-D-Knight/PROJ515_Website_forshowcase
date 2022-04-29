f81e03b42e802ba9196676d169f8491f39677048
Steve-D-Knight <72265339+Steve-D-Knight@users.noreply.github.com>
2022-04-29 18:19:14 +0100

    new image

--- breadboard.js
+++ breadboard.js
@@ -1,6 +1,6 @@
 var breadboard_page = '<div>Housing Page</div>';
 
-var image3 = '<img height="500px" src="images/breadboard.jpg" alt="LoRaWAN Mesh network node render">';
+var image3 = '<img src="images/breadboard.jpg" alt="LoRaWAN Mesh network node render">';
 
 breadboard_page = '<div><table><tr><td><div>' + image3 + '</div></td><td width="500px" id="whitespace"><div>LoRaWAN using a device-relay Using low cost, off the shelf sensors and components, we are looking to create a cheaper alternative to manual data acquisition. At the core of our project, we are using an STM32-WL55JC1 microprocessor. Sensors are being programmed into the system via SPI or I2C. Current sensors in use include gaseous temperature, humidity, pressure and altitude. Expanding further, we have an interest in methane and carbon dioxide. </div></td><tr></table></div>';
 
